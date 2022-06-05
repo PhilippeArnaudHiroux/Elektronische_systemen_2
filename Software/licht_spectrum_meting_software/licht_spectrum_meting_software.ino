3cbf36c169f01b1cc26590d7cb18baa78afdd898
Bram Vanderwegen <12001742@student.pxl.be>
2022-06-06 00:22:46 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ Software/licht_spectrum_meting_software/licht_spectrum_meting_software.ino
@@ -0,0 +1,49 @@
+#define pin_A0 8
+#define pin_A1 9
+#define pin_A2 10
+
+#define pin_EN 11
+#define pin_ADC 7
+
+int buffer_ = 0;
+
+int data_array[8];
+
+void write_data(char number);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin_A0, OUTPUT);
+  pinMode(pin_A1, OUTPUT);
+  pinMode(pin_A2, OUTPUT);
+  pinMode(pin_EN, OUTPUT);
+  pinMode(pin_ADC, INPUT);
+  digitalWrite(pin_EN, LOW);
+  Serial.begin(9600);
+  Serial.println("########");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  char count = 0;
+  for(int i = 0; i < 8; i++){
+    write_data(count);
+    delay(10);
+    data_array[i] = analogRead(pin_ADC);
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(data_array[i]);
+    
+    //Serial.println("##");
+    delay(10);
+    count++;
+  }
+  Serial.println("########");
+  delay(1000);
+}
+
+void write_data(char number){
+  digitalWrite(pin_A0, number & 1);
+  digitalWrite(pin_A1, (number & 2) >> 1);
+  digitalWrite(pin_A2, (number & 4) >> 2);
+}
